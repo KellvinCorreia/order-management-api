73937718c5d4e07887f2f9fe2ed4efa17a4325e0
Kellvin Correia Alves <kellvin.correia@gmail.com>
2026-01-14 02:18:08 -0300

    Feat: Add Clickjacking protection
    - Implement X-Frame-Options DENY middleware
    
    - Implement CSP frame-ancestors 'none' middleware

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+// Middleware de Proteção conta Iframe (Clickjacking)
+app.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
+  next();
+});
+
 app.use('/api', routes);
 
 app.listen(port, () => {
